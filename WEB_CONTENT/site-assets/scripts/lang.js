a9a02f343a39ea6fe42b0134d067f010966c3593
enricoviolagit <enricoviola82@gmail.com>
2026-06-05 11:19:33 +0200

    referto di Lorenzo Viola

---
+++ WEB_CONTENT/site-assets/scripts/lang.js
@@ -0,0 +1,38 @@
+﻿var defaultLanguage = 'it-IT';
+var languages =
+{
+    "it-IT":
+    {
+        show_digitally_signed_report: "Mostra referto firmato digitalmente",
+        show_report: "Mostra referto",
+        patient: "Paziente",
+        dob: "Data di nascita",
+        dicom_button: "Avvia visualizzatore DICOM",
+        html_button: "Avvia visualizzatore HTML (consigliato Chrome)",
+        select_study: "Selezionare lo studio/serie dall&#39;elenco a sinistra",
+        chrome_recommended: "Si consiglia l&#39;uso del browser "
+	},
+    "en-GB":
+    {
+        show_digitally_signed_report: "View digitally signed report",
+        show_report: "View report",
+        patient: "Patient",
+        dob: "DoB",
+        dicom_button: "Start DICOM viewer",
+        html_button: "Start HTML viewer (Chrome recommended)",
+        select_study: "Select study/series from the list on the left",
+        chrome_recommended: "Recommended browser: "
+    },
+    "ru-RU":
+    {
+        show_digitally_signed_report: "Просмотр отчета с цифровой подписью",
+        show_report: "Просмотр отчета",
+        patient: "Пациент",
+        dob: "Дата рождения",
+        dicom_button: "Запустить программу просмотра DICOM",
+        html_button: "Запустить просмотрщик HTML (рекомендуется Chrome)",
+        select_study: "Выберите исследование / серию из списка слева",
+        chrome_recommended: "Рекомендуемый браузер: "
+	}
+};
+
